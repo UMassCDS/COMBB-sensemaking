c00f4f7c84d0eb269ef8fa2d6e9568dab054189a
bwcompton <bcompton@umass.edu>
2024-08-08 16:53:19 -0400

    Fix dygraphs bug--it was throwing warnings, and crashing up on the server

--- buzzbay.app.R
+++ buzzbay.app.R
@@ -66,7 +66,8 @@ server <- function(input, output, session) {
    
    
    observeEvent(input$siteYear, {
-      session$userData$sensor <- sensors[sensors$siteYear == input$siteYear, ]
+    print('siteyear triggered')
+        session$userData$sensor <- sensors[sensors$siteYear == input$siteYear, ]
       zzz <<- session$userData$sensor
       minmax <- c(min(session$userData$sensor$Date_Time), max(session$userData$sensor$Date_Time))
       updateSliderInput('period', min = minmax[1], max = minmax[2], value = minmax,
@@ -77,8 +78,10 @@ server <- function(input, output, session) {
    })
    
    observeEvent(list(input$siteYear, input$period), {
+      print('siteyear and period triggered')
       session$userData$window <- session$userData$sensor[session$userData$sensor$Date_Time >= input$period[1] & 
                                                             session$userData$sensor$Date_Time <= input$period[2], ]   
+      cat('=== window = ', paste(dim(session$userData$window), collapse = ', '), ' ===\n', sep = '')
       session$userData$keep.date.window <- FALSE
    })
    
@@ -91,19 +94,50 @@ server <- function(input, output, session) {
    })
    
    observeEvent(list(input$siteYear, input$period, input$units, input$grab.bag), {
-      vars <- session$userData$window[, c('Date_Time', unit.vars[as.integer(input$units)])]
-      #  vars <- xts(vars[,-1], vars[,1])
-      output$plot <- renderDygraph({
-         dygraph(vars, main = 'Dissolved oxygen', ylab = unit.names[as.integer(input$units)]) |>
-            dyAxis('x', gridLineColor = '#D0D0D0') |>
-            dyAxis('y', gridLineColor = '#D0D0D0') |>
-            dySeries(color = '#3C2692') |>
-            #    dySeries('grab.bag', drawPoints = TRUE, strokeWidth = 0) |>     # this is how we'll do grab-bag points
-            dyRangeSelector(retainDateWindow = session$userData$keep.date.window) |>
-            dyUnzoom() |>
-            dyCrosshair(direction = "vertical")
-      })
-   })
+      cat('--- plot triggered\n')
+      if(dim(session$userData$window)[1] > 0) {
+         vars <- session$userData$window[, c('Date_Time', unit.vars[as.integer(input$units)])]
+         #  vars <- xts(vars[,-1], vars[,1])
+         cat('--- siteYear:\n')
+         print(dim(input$siteYear))
+         cat('--- period:\n')
+         print(input$period)
+         cat('--- units:\n')
+         print(input$units)
+         
+         
+         
+         #vars <- data.frame(1:1000, runif(1000))
+         #vars <- head(vars)
+         
+         #vars <- head(vars)
+         
+         
+         #      cat('converting to POSIXct\n')
+         # #     vars$Date_Time <- as.POSIXct(vars$Date_Time)
+         #    #  vars <- head(vars)
+         #      cat('here it is\n')
+         #      print(head(vars))
+         #      cat('converting to xts\n')
+         #    #  vars <- xts(vars$DO, vars$Date_Time)          # not needed it seems
+         #      cat('here it is as xts\n')
+         #      print(head(vars))
+         #      cat('lets go!\n')
+         #      
+         #      vars <<- vars
+         #      
+         
+         output$plot <- renderDygraph({
+            dygraph(vars, main = 'Dissolved oxygen', ylab = unit.names[as.integer(input$units)]) |>
+               dyAxis('x', gridLineColor = '#D0D0D0') |>
+               dyAxis('y', gridLineColor = '#D0D0D0') |>
+               dySeries(color = '#3C2692') |>
+               #    dySeries('grab.bag', drawPoints = TRUE, strokeWidth = 0) |>     # this is how we'll do grab-bag points
+               dyRangeSelector(retainDateWindow = session$userData$keep.date.window) |>
+               dyUnzoom() |>
+               dyCrosshair(direction = "vertical")
+         })
+      }})
 }   
 
 
