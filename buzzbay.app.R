904d79504c3758e01c9c6faca2a7724f50a72e41
bwcompton <bcompton@umass.edu>
2024-08-09 14:01:10 -0400

    Simplify and clean up

--- buzzbay.app.R
+++ buzzbay.app.R
@@ -25,7 +25,7 @@ sensors <- readRDS('inst/sensors.RDS')
 
 # User interface ---------------------
 ui <- page_sidebar(
-   theme = bs_theme(bootswatch = 'cerulean', version = 5),   # bslib version defense. Use version_default() to update
+   theme = bs_theme(bootswatch = 'cerulean', version = 5),     # bslib version defense. Use version_default() to update
    
    title = 'buzzbay (dev version)',
    
@@ -61,63 +61,49 @@ ui <- page_sidebar(
 # Server -----------------------------
 server <- function(input, output, session) {
    
-   #bs_themer()                                 # uncomment to select a new theme
+   #bs_themer()                                                # uncomment to select a new theme
    #  print(getDefaultReactiveDomain())
    
    
-   observe({      # observeEvent(input$siteYear, {
-      print('siteyear triggered')
-      session$userData$window <- session$userData$sensor <- sensors[sensors$siteYear == input$siteYear, ]
-      cat('sensors = ', dim(session$userData$sensor)[1], '\n', sep = '')
-      cat('window = ', dim(session$userData$window)[1], '\n', sep = '')
-      cat('---\n')
-      zzz <<- session$userData$sensor
+   observe({                                                   # --- New site/year selected. Update time period slider
+      session$userData$sensor <- sensors[sensors$siteYear == input$siteYear, ]
       minmax <- c(min(session$userData$sensor$Date_Time), max(session$userData$sensor$Date_Time))
       updateSliderInput('period', min = minmax[1], max = minmax[2], value = minmax,
-                        timeFormat = '%b %e',
-                        session = getDefaultReactiveDomain())
+                        timeFormat = '%b %e', session = getDefaultReactiveDomain())
       session$userData$keep.date.window <- FALSE
-      
    })
    
-   # observeEvent(list(input$siteYear, input$period), {
-   #    print('siteyear and period triggered')
-   #    session$userData$window <- session$userData$sensor[session$userData$sensor$Date_Time >= input$period[1] & 
-   #                                                          session$userData$sensor$Date_Time <= input$period[2], ] 
-   #    cat('period = ', input$period[1], ' - ', input$period[2], '\n', sep =)
-   #    cat('window = ', dim(session$userData$window)[1], '\n', sep = '')
-   #    cat('---\n')
-   #    session$userData$keep.date.window <- FALSE
-   # })
+   observeEvent(input$period, {                                # --- Time period changed, so reset date window
+      session$userData$keep.date.window <- FALSE
+   })
    
-   observeEvent(input$units, {
+   observeEvent(input$units, {                                 # --- Units changed, so keep date window
       session$userData$keep.date.window <- TRUE
    })
    
-   observeEvent(input$grab.bag, {                        
+   observeEvent(input$grab.bag, {                              # --- Grab bag. Keep date window        
       # nothing here yet
       session$userData$keep.date.window <- TRUE
    })
    
-   observeEvent(list(input$siteYear, input$period, input$units, input$grab.bag), {
-      session$userData$window <- session$userData$sensor[session$userData$sensor$Date_Time >= input$period[1] & 
-                                                            session$userData$sensor$Date_Time <= input$period[2], ] 
-      cat('--- plot triggered ', dim(session$userData$window)[1], '\n', sep = '')
-      if(dim(session$userData$window)[1] > 0) {
-         vars <- session$userData$window[, c('Date_Time', unit.vars[as.integer(input$units)])]
-         
-         output$plot <- renderDygraph({
-            dygraph(vars, main = 'Dissolved oxygen', ylab = unit.names[as.integer(input$units)]) |>
-               dyAxis('x', gridLineColor = '#D0D0D0') |>
-               dyAxis('y', gridLineColor = '#D0D0D0') |>
-               dySeries(color = '#3C2692') |>
-               #    dySeries('grab.bag', drawPoints = TRUE, strokeWidth = 0) |>     # this is how we'll do grab-bag points
-               dyRangeSelector(retainDateWindow = session$userData$keep.date.window) |>
-               dyUnzoom() |>
-               dyCrosshair(direction = "vertical")
-         })
+   observeEvent(list(input$siteYear, input$period, input$units, input$grab.bag), {   # --- Draw the plot
+      vars <- session$userData$sensor[session$userData$sensor$Date_Time >= input$period[1] &
+                                         session$userData$sensor$Date_Time <= input$period[2],
+                                      c('Date_Time', unit.vars[as.integer(input$units)])]
+      cat(dim(vars), '\n')
+      if(dim(vars)[1] > 0) {
+      output$plot <- renderDygraph({
+         dygraph(vars, main = 'Dissolved oxygen', ylab = unit.names[as.integer(input$units)]) |>
+            dyAxis('x', gridLineColor = '#D0D0D0') |>
+            dyAxis('y', gridLineColor = '#D0D0D0') |>
+            dySeries(color = '#3C2692') |>
+            #    dySeries('grab.bag', drawPoints = TRUE, strokeWidth = 0) |>     # this is how we'll do grab-bag points
+            dyRangeSelector(retainDateWindow = session$userData$keep.date.window) |>
+            dyUnzoom() |>
+            dyCrosshair(direction = "vertical")
+      })
       }
-   }    ) #       , ignoreInit = TRUE)
+   })
 }   
 
 
