55344d90daf0d373345739e3ae40b254958b90f7
bwcompton <bcompton@umass.edu>
2024-08-12 18:46:35 -0400

    get reactive right for period; add rest of inputs and UMass logo

--- buzzbay.app.R
+++ buzzbay.app.R
@@ -9,6 +9,7 @@ library(shinybusy)
 library(shinyWidgets)
 library(dygraphs)
 library(lubridate)
+library(shinyjs)
 
 
 source('dygraphs_plugins.R')
@@ -22,11 +23,17 @@ sites <- sites[sites$include == TRUE,]
 siteYear <- readRDS('inst/siteYear.RDS')
 all.sensors <- readRDS('inst/sensors.RDS')
 
+aggreg.choices = list('None' = 0, 'Hourly' = 1, '4 hours' = 4, '8 hours' = 8, '12 hours' = 12, 'Daily' = 24, 
+                      'Weekly' = 7 * 24, 'Bi-weekly' = 14 * 24, '30 days' = 30 & 24, 'Entire period' = 1e6)
+method.choices = c('Mean', 'Minimum', '5th percentile', '10th percentile', 'Median', 
+                  'Maximum', 'Standard deviation', 'Percent exceedence')
+
 
 
 # User interface ---------------------
 ui <- page_sidebar(
    theme = bs_theme(bootswatch = 'cerulean', version = 5),     # bslib version defense. Use version_default() to update
+   useShinyjs(),
    
    title = 'buzzbay (dev version)',
    
@@ -35,19 +42,35 @@ ui <- page_sidebar(
          add_busy_spinner(spin = 'fading-circle', position = 'top-left', onstart = FALSE, timeout = 500),
          
          card(
-            selectInput('siteYear', label = 'Site', choices = siteYear$siteYear),
-            sliderInput('period', label = 'Time period', min = 0, max = 1e12, value = c(0, 1e12)),
+            selectInput('siteYear', label = 'Site and year', choices = siteYear$siteYear),
+            
+            sliderInput('period', label = 'Time period', min = 0, max = 0, value = c(0, 0)),
+            
             radioButtons('units', label = 'Units', choiceNames = as.list(unit.names), choiceValues = 1:2),
             
-            textInput('threshold', label = 'Comparison threshold', value = '', placeholder = 'n.nn',
-                      width = '40%'),   
+            hr(),
+            
+            numericInput('threshold', label = 'Comparison threshold', value = '',
+                         step = 0.1, width = '40%'),   
             materialSwitch(inputId = 'plot.threshold', label = 'Plot comparison threshold', 
                            value = FALSE),
-            textInput('exceedance', label = 'Exceedance threshold', value = '', placeholder = 'nn',
-                      width = '40%'),
+            numericInput('exceedance', label = 'Exceedance threshold (%)', value = '',
+                         step = 1, width = '40%'),
             materialSwitch(inputId = 'grab.bag', label = 'Display grab-bag samples', 
                            value = FALSE),
-            numericInput('test', label = 'Numeric input test', step = 0.1, value = 5.0)
+            
+            selectInput('interval', label = 'Aggregation interval', choices = aggreg.choices),
+            
+            selectInput('method', label = 'Aggregation method', choices = method.choices, 
+                        selected = 'median'),
+            
+            materialSwitch(inputId = 'moving.window', label = 'Moving window'),
+            
+            hr(),
+            
+            actionLink('aboutSite', label = 'About this site'),
+            br(),
+            tags$img(height = 103, width = 178, src = 'umass_logo.png')
          ),
          width = 340
       ),
@@ -66,33 +89,39 @@ server <- function(input, output, session) {
    #bs_themer()                                                # uncomment to select a new theme
    #  print(getDefaultReactiveDomain())
    
+   disable('period')                                           # this is dim while it shows 0,0
+   
    
    observe({                                                   # --- New site/year selected. Update time period slider
       session$userData$sensor <- all.sensors[all.sensors$siteYear == input$siteYear, ]
       minmax <- c(min(session$userData$sensor$Date_Time), max(session$userData$sensor$Date_Time))
       updateSliderInput('period', min = minmax[1], max = minmax[2], value = minmax,
                         timeFormat = '%b %e', session = getDefaultReactiveDomain())
+      enable('period')
       session$userData$keep.date.window <- FALSE
    })
    
-   observeEvent(input$period, {                                # --- Time period changed, so reset date window
-      session$userData$period <- as.POSIXct(floor_date(as.POSIXct(input$period) - 4 * 60 * 60, 'days')) + 4 * 60 * 60  # round to midnight (adjusted for EDT)
-      session$userData$keep.date.window <- FALSE
-   })
    
    observeEvent(input$units, {                                 # --- Units changed, so keep date window
       session$userData$keep.date.window <- TRUE
    })
+   
    
    observeEvent(input$grab.bag, {                              # --- Grab bag. Keep date window        
       # nothing here yet
       session$userData$keep.date.window <- TRUE
    })
    
-   observeEvent(list(input$siteYear, input$period, input$units, input$grab.bag), {   # --- Draw the plot
-      vars <- session$userData$sensor[session$userData$sensor$Date_Time >= session$userData$period[1] &
-                                         session$userData$sensor$Date_Time <= session$userData$period[2],
+   
+   observeEvent(list(input$siteYear, input$period, input$units, input$grab.bag), {     # --- Draw the plot
+      period <- as.POSIXct(floor_date(as.POSIXct(input$period) - 4 * 60 * 60, 'days')) + 4 * 60 * 60  # round to midnight (adjusted for EDT)
+      vars <- session$userData$sensor[session$userData$sensor$Date_Time >= period[1] &
+                                         session$userData$sensor$Date_Time <= period[2],
                                       c('Date_Time', unit.vars[as.integer(input$units)])]
+      
+      if(!identical(input$period, session$userData$period))    # if time period changed, reset date window
+         session$userData$keep.date.window <- FALSE
+      session$userData$period <- input$period
       
       if(dim(vars)[1] > 0) {
          output$plot <- renderDygraph({
@@ -101,7 +130,7 @@ server <- function(input, output, session) {
                dyAxis('x', gridLineColor = '#D0D0D0') |>
                dyAxis('y', gridLineColor = '#D0D0D0') |>
                dySeries(color = '#3C2692') |>
-               #    dySeries('grab.bag', drawPoints = TRUE, strokeWidth = 0) |>     # this is how we'll do grab-bag points
+               #    dySeries('grab.bag', drawPoints = input$grab.bag, strokeWidth = 0) |>     # this is how we'll do grab-bag points
                dyRangeSelector(retainDateWindow = session$userData$keep.date.window) |>
                dyUnzoom() |>
                dyCrosshair(direction = "vertical")
