cc6cedc449dcc736a039868ec0edab84005a5747
bwcompton <bcompton@umass.edu>
2024-11-20 12:17:55 -0500

    Incorporated new data, which has lots of formatting issues that are different from the previous version's formatting issues. Lots of changes to gather.data, plus a bit of hand-editing (see gather.data). New datasets are incorporated and seem to work.

--- gather.data.R
+++ gather.data.R
@@ -20,11 +20,19 @@
    #                             Grab_DO           DO from grab sensors (mg/L)
    # Notes: (1) if a sensor/grab sample file matches more than one column name variant, the first one
    #            listed in col.names will be chosen. (This is what happens when your data are a mess. Grr.)
-   #        (2) sensor files are .xlsx, grab sensor files are .xls
+   #        (2) sensor files are .xlsx, grab sensor now are too
    #        (3) grab sensor files have two TIME columns. I'm just using the first - I think they're the same.   
    #        (4) grab sensor data don't have negative values, so I'm not cleaning these up
    #        (5) sensor and grab sensor data are combined, with shared site/year and date/time columns. Sensor DO 
    #            values are imputed to prevent breaks in sensor data lines when plotted; will be dropped when calculating
+   #
+   # Data update 20 Nov 2024: The new datasets have a number of new issues, some of which I fixed by hand:
+   #        (1) previously, files were split between sensor and grab data, with each site in a separate folder. 
+   #            New files have both datasets in the same spreadsheet. I split them to match the old version, in
+   #            a likely misguided move, I hand-edited rather than changing the code.
+   #        (2) different column names for both sensor and grab, now incorporated in col.names, now used for both.
+   #        (3) AB2 has multiple spaces in the time for the first sample. I edited it by hand.
+   #        (4) WHX1 has two blank lines, and the site name column stops after one of them. Hand-edited.
    # B. Compton, 31 Jul 2024
    
    
@@ -34,12 +42,26 @@
    library(lubridate)
    
    
-   col.names <- list(list('Site_Year', 'Site_Year'),              # standard col name, variants
-                     list('Date_Time', c('SAMP_DATE_TIME', 'Date Time')), 
-                     list('DO', c('DO_MGL2', 'DO_mgl', 'DO_MGL')))
+   col.names <- list(list('Site_Year', 'Site_Year', 'Site'),      # standard col name, variants. Now used for both sensor and grab sample data.
+                     list('Date_Time', c('SAMP_DATE_TIME', 'Date Time', 'Sensor Date Time', 'Sensors Date Time', 'Baywatchers Date_Time')), 
+                     list('DO', c('DO_MGL2', 'DO_mgl', 'DO_MGL', 'DO (mg/L)', 'DO  (mg/L)')))
    
    
    'fmt.date' <- function(x) substr(format(as.POSIXct(x)), 1, 10)
+   
+   
+   'fix.column.names' <- function(n, col.names) {                 # standardize mangled column names
+      c <- rep(NA, length(col.names))
+      
+      for(k in 1:length(col.names)) {
+         d <- match(col.names[[k]][[2]], n)                       #       match possible name variants
+         d <- d[!is.na(d)]
+         if(length(d) == 0) stop('Column ', col.names[[k]][[1]], ' not found\n')
+         c[k] <- d[1]                                             #       first matching column name
+      }
+      c
+   }
+   
    
    
    sites <- read.csv('inst/sitenames.csv')
@@ -68,21 +90,12 @@
          x <- suppressWarnings(read_excel(paste0(path, f), sheet = 1))
          x$Site_Year <- paste0(sites$description[i], ' - ', j)
          
-         n <- names(x)                                            #       find columns
-         c <- rep(NA, length(col.names))
-         
-         for(k in 1:length(col.names)) {
-            d <- match(col.names[[k]][[2]], n)                    #       match possible name variants
-            d <- d[!is.na(d)]
-            if(length(d) == 0) stop('Column ', col.names[[k]][[1]], ' not found\n')
-            c[k] <- d[1]                                          #       first matching column name
-         }
-         
+         c <- fix.column.names(names(x), col.names)               # fix chaotic column names
          
          t <- data.frame(standard = q, found.column = names(x)[c])
          print.data.frame(t[-1,], row.names = FALSE)
          
-         x <- x[, c]                   # don't let rbind's miserable name matching mess us up
+         x <- x[, c]                                              # don't let rbind's miserable name matching mess us up
          names(x) <- q
          
          b <- sum(x$DO < 0, na.rm = TRUE)                         # set negative values to NA
@@ -99,18 +112,22 @@
          
          
          # --- Now, get grab sensor data ---
-         f <- paste0(sites$site[i], '_grab_', j, '.xls')          #     grab sensor filename. grab sensor files have consistent column names (so far)
+         f <- paste0(sites$site[i], '_grab_', j, '.xlsx')         #     grab sensor filename. grab sensor files have consistent column names (so far)
          cat('grab sensor data file: ', f, '\n', sep = '')
          x <- suppressWarnings(read_excel(paste0(path, f), sheet = 1, .name_repair = 'minimal'))  #       there are 2 time columns. We just want the first one
          x$Site_Year <- paste0(sites$description[i], ' - ', j)
+              
+         c <- fix.column.names(names(x), col.names)               # fix chaotic column names
+         x <- x[, c]                                              # don't let rbind's miserable name matching mess us up
+         names(x) <- q
          
-         x$Date_Time <- paste(unlist(lapply(x$SAMP_DATE, substr, 1, 10)), unlist(lapply(x$TIME, substr, 12, 99))) #       combine date and time
+      #   x$Date_Time <- paste(unlist(lapply(x$SAMP_DATE, substr, 1, 10)), unlist(lapply(x$TIME, substr, 12, 99))) #       combine date and time
          
-         x <- x[,c('Site_Year', 'Date_Time', 'DO_MGL')]            #       pull columns in correct order
+         x <- x[,c('Site_Year', 'Date_Time', 'DO')]               #       pull columns in correct order
          names(x) <- unlist(lapply(col.names, `[[`, 1))           #       and apply standard names
          
          suppressWarnings(x$DO <- as.numeric(x$DO))               #       these come in as character
-         
+            
          cat(format(dim(x)[1], big.mark = ','), ' cases; date range = ', fmt.date(min(x$Date_Time)), ' - ', fmt.date(max(x$Date_Time)), '\n', sep = '')
          
          x <- x[x$Date_Time >= date.range[1] & x$Date_Time <= date.range[2], ]
@@ -126,13 +143,13 @@
    saveRDS(sy, 'inst/Site_Year.RDS')
    
    
-   # date.range <- c(max(aggregate(Date_Time ~ Site_Year, data = z, FUN = min)$Date_Time),           # trim sensor and grab data to common sensor date range 
-   #                 min(aggregate(Date_Time ~ Site_Year, data = z, FUN = max)$Date_Time))
-   # cat('Trimming entire dataset to common sensor date range ', fmt.date(date.range[1]), ' - ' , fmt.date(date.range[2]), '\n', sep = '')
-   # z <- z[z$Date_Time >= date.range[1] & z$Date_Time <= date.range[2], ]
-   # g <- g[g$Date_Time >= date.range[1] & g$Date_Time <= date.range[2], ]
-   # cat(dim(z)[1], ' rows left in sensor data, ', dim(g)[1], ' rows left in grab sample data.\n', sep = '')
-
+   date.range <- c(max(aggregate(Date_Time ~ Site_Year, data = z, FUN = min)$Date_Time),           # trim sensor and grab data to common sensor date range
+                   min(aggregate(Date_Time ~ Site_Year, data = z, FUN = max)$Date_Time))
+   cat('Trimming entire dataset to common sensor date range ', fmt.date(date.range[1]), ' - ' , fmt.date(date.range[2]), '\n', sep = '')
+   z <- z[z$Date_Time >= date.range[1] & z$Date_Time <= date.range[2], ]
+   g <- g[g$Date_Time >= date.range[1] & g$Date_Time <= date.range[2], ]
+   cat(dim(z)[1], ' rows left in sensor data, ', dim(g)[1], ' rows left in grab sample data.\n', sep = '')
+   
    
    
    # Combine sensor and grab sensor data into a single data frame, with Site_Year, Date_Time, DO, Grab_DO, and Source
